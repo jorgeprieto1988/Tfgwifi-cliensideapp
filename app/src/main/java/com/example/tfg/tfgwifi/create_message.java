58ee124acb5760c9cc32924350f041795181c2c0
trokotrokos <trokotrokos@hotmail.com>
2017-06-05 20:22:00 +0200

    Crear y enviar mensajes desde la app

--- app/src/main/java/com/example/tfg/tfgwifi/create_message.java
+++ app/src/main/java/com/example/tfg/tfgwifi/create_message.java
@@ -12,8 +12,23 @@ import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 /**
  * Created by troko on 01/06/2017.
@@ -27,8 +42,53 @@ public class create_message extends Activity {
 
         setContentView(R.layout.create_message);
         Button clickButton_mycoords = (Button) findViewById(R.id.bt_mi_pos);
+        Button clickButton_enviar = (Button) findViewById(R.id.bt_enviar_msg);
         final TextView et_latitud = (TextView) findViewById(R.id.et_latitud);
         final TextView et_longitud = (TextView) findViewById(R.id.et_longitud);
+        final TextView et_mensaje = (TextView) findViewById(R.id.et_mensaje);
+        final TextView et_tiempo = (TextView) findViewById(R.id.et_tiempo);
+        final TextView et_area = (TextView) findViewById(R.id.et_area);
+
+        clickButton_enviar.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+
+                try {
+                    String contenido = URLEncoder.encode(et_mensaje.getText().toString(), "UTF-8");
+
+                    String urljson = "http://tfg-wifi.appspot.com/setmessage?contenido=" + contenido +
+                            "&area_mensaje=" + et_area.getText() +
+                            "&tiempo_vida" + et_tiempo.getText() +
+                            "&latitud=" + et_latitud.getText() + "&longitud=" + et_longitud.getText();
+
+                    JsonArrayRequest jsarrayRequest = new JsonArrayRequest
+                            (Request.Method.GET, urljson, null, new Response.Listener<JSONArray>() {
+
+                                @Override
+                                public void onResponse(JSONArray response) {
+                                    et_mensaje.setText("Response: " + response.toString());
+                                    //  lista_mensajes = new ArrayList<String>();
+
+                                }
+                            }, new Response.ErrorListener() {
+
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    // TODO Auto-generated method stub
+                                    et_mensaje.setText("Error:" + error.toString());
+                                }
+                            });
+                    RequestQueue queue = Volley.newRequestQueue(create_message.this);
+                    queue.add(jsarrayRequest);
+
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        });
         clickButton_mycoords.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
